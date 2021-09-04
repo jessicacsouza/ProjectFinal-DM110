92d0086f7b8e5ffd65ad465b3111e05916152197
jessicacsouza <jessicasouza@gec.inatel.br>
2021-09-03 23:55:51 -0300

    Implementação CustomerService

--- trabalho-impl/src/main/java/br/inatel/dm110/impl/CustomerServiceImpl.java
+++ trabalho-impl/src/main/java/br/inatel/dm110/impl/CustomerServiceImpl.java
@@ -1,7 +1,30 @@
 package br.inatel.dm110.impl;
 
+import java.util.List;
+
+import javax.ws.rs.core.Response;
+
 import br.inatel.dm110.api.CustomerService;
+import br.inatel.dm110.api.CustomerTO;
 
 public class CustomerServiceImpl implements CustomerService {
+
+	@Override
+	public List<CustomerTO> listCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Response getCustomer(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CustomerTO insertCustomer(CustomerTO cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
